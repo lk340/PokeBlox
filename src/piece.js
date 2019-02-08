ae33a15e044e706cb8285223953b222554d9fdb0
Loyd Kim <loyd.hkim@gmail.com>
2019-02-08 14:40:49 -0500

    give each grid a class #

--- src/piece.js
+++ src/piece.js
@@ -4,6 +4,32 @@ export default class Piece {
   }
 
   piece() {
+    if (this.type === "I") {
 
+    }
+
+    else if (this.type === "O") {
+
+    }
+
+    else if (this.type === "T") {
+      
+    }
+
+    else if (this.type === "S") {
+      
+    }
+
+    else if (this.type === "Z") {
+      
+    }
+
+    else if (this.type === "J") {
+      
+    }
+
+    else if (this.type === "L") {
+      
+    }
   }
 }
