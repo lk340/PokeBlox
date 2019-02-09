994b1c94250546f9b660034dc5db5fb82eb1fcfc
Loyd Kim <loyd.hkim@gmail.com>
2019-02-09 15:51:19 -0500

    Finish logic for keeping pieces in the board when moving down

--- src/game.js
+++ src/game.js
@@ -93,10 +93,16 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     else if (event.which === 83) {
-    // else if (event.which === 83) {
       // s key
-      if (parseInt(document.getElementById(`tetronimo-${currentPiece}`).style.marginTop) <= 540) {
-        document.getElementById(`tetronimo-${currentPiece}`).style.marginTop = `${parseInt(document.getElementById(`tetronimo-${currentPiece}`).style.marginTop) + 30}px`;
+      if (currentPiece === "I") {
+        if (parseInt(document.getElementById(`tetronimo-${currentPiece}`).style.marginTop) <= 540) {
+          document.getElementById(`tetronimo-${currentPiece}`).style.marginTop = `${parseInt(document.getElementById(`tetronimo-${currentPiece}`).style.marginTop) + 30}px`;
+        }
+      }
+      else {
+        if (parseInt(document.getElementById(`tetronimo-${currentPiece}`).style.marginTop) <= 510) {
+          document.getElementById(`tetronimo-${currentPiece}`).style.marginTop = `${parseInt(document.getElementById(`tetronimo-${currentPiece}`).style.marginTop) + 30}px`;
+        }
       }
     }
 
